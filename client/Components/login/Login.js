7a1897b44deecb488bd2bbb6332147b0016969e6
jthagerman <jthagerma@gmail.com>
2021-08-21 11:04:01 -0400

    worked on cleaning up inline styles and setting up state in login and signup

--- client/Components/login/Login.js
+++ client/Components/login/Login.js
@@ -1,72 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   StyleSheet,
   Text,
   View,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
   SafeAreaView,
   TouchableHighlight,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
   TextInput,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import Banner from "./Banner";
 
-
-
 export default function Login() {
+  const [username, setUsername] = React.useState();
+  const [password, setPassword] = React.useState();
 
   return (
-    <SafeAreaView style={{ flex: 0, backgroundColor: "white", height: "100%"  }}>
+    <SafeAreaView style={{ flex: 0, backgroundColor: "white", height: "100%" }}>
       <Banner></Banner>
-      <View
-        style={{
-          width: "100%",
-          flex: 0,
-          alignContent: "center",
-          textAlign: "left",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: -50,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 40,
-            color: "#ED3B5B",
-            textAlign: "left",
-            marginBottom: 20,
-            width: "80%",
-          }}
-        >
-          Login
-        </Text>
-        <TextInput placeholder="username" style={styles.input}></TextInput>
-        <TextInput placeholder="password" style={styles.input}></TextInput>
+      <View style={styles.view}>
+        <Text style={styles.loginText}>Login</Text>
+        <TextInput placeholder="username" 
+          style={styles.input}
+          value={username}
+          maxLength={30}
+          onChangeText={(text) => setUsername(text)}></TextInput>
+
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          style={styles.input}
+          value={password}
+          maxLength={30}
+          onChangeText={(text) => setPassword(text)}
+        ></TextInput>
 
         <TouchableHighlight
-          style={{ width: "50%", backgroundColor: "#ED3B5B", borderRadius: 45, marginTop: 20 }}
-
+          style={styles.loginButton}
+          onPress={() => alert(`username: ${username} password: ${password}`)}
         >
-          <Text
-            style={{
-              fontSize: 25,
-              color: "white",
-              padding: 16,
-              textAlign: "center",
-            }}
-          >
-            Login
-          </Text>
+          <Text style={styles.loginButtonText}>Login</Text>
         </TouchableHighlight>
-
       </View>
     </SafeAreaView>
   );
@@ -82,5 +56,35 @@ const styles = StyleSheet.create({
     width: "90%",
     borderRadius: 999,
     paddingLeft: 20,
+  },
+  view: {
+    width: "100%",
+    flex: 0,
+    alignContent: "center",
+    textAlign: "left",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: -50,
+  },
+  loginText: {
+    fontSize: 40,
+    color: "#ED3B5B",
+    textAlign: "left",
+    marginBottom: 20,
+    width: "80%",
+  },
+
+  loginButton: {
+    width: "50%",
+    backgroundColor: "#ED3B5B",
+    borderRadius: 45,
+    marginTop: 20,
+  },
+
+  loginButtonText: {
+    fontSize: 25,
+    color: "white",
+    padding: 16,
+    textAlign: "center",
   },
 });
--- client/Components/login/Signup.js
+++ client/Components/login/Signup.js
@@ -1,68 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   StyleSheet,
   Text,
   View,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
   SafeAreaView,
   TouchableHighlight,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
   TextInput,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import Banner from "./Banner";
 
 export default function Signup() {
-  return (
-    <SafeAreaView style={{ flex: 0, backgroundColor: "white", height: "100%", paddingTop: 25}}>
-      <Banner></Banner>
-      <View
-        style={{
-          width: "100%",
-          flex: 0,
-          alignContent: "center",
-          textAlign: "left",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: -50,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 40,
-            color: "#ED3B5B",
-            textAlign: "left",
-            marginBottom: 20,
-            width: "80%",
-          }}
-        >
-          Signup
-        </Text>
-        <TextInput placeholder="username" style={styles.input}></TextInput>
-        <TextInput placeholder="email" style={styles.input}></TextInput>
-        <TextInput placeholder="phone number" style={styles.input}></TextInput>
-        <TextInput placeholder="password" style={styles.input}></TextInput>
-        <TouchableHighlight
-          style={{ width: "50%", backgroundColor: "#ED3B5B", borderRadius: 45, marginTop: 20 }}
+  const [username, setUsername] = React.useState();
+  const [email, setEmail] = React.useState();
+  const [phone, setPhone] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [check, setCheck] = React.useState();
 
+  const clickSubmit = () => {
+    if (!email || !username || !phone || !password || !check) {
+      alert("all fields must be filled");
+    } else if (check !== password) {
+      alert("passwords do not match");
+    } else {
+      alert("account made");
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Banner></Banner>
+      <View style={styles.form}>
+        <Text style={styles.formTitle}>Create a new account</Text>
+        <TextInput
+          placeholder="Username"
+          style={styles.input}
+          maxLength={30}
+          onChangeText={(text) => setUsername(text)}
+          textContentType={"username"}
+        ></TextInput>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          onChangeText={(text) => setEmail(text)}
+          textContentType={"emailAddress"}
+        ></TextInput>
+        <TextInput
+          placeholder="Phone Number"
+          style={styles.input}
+          onChangeText={(text) => setPhone(text)}
+          keyboardType={"phone-pad"}
+          textContentType={"telephoneNumber"}
+        ></TextInput>
+        <TextInput
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          textContentType={"password"}
+          style={styles.input}
+          secureTextEntry={true}
+          maxLength={30}
+        ></TextInput>
+        <TextInput
+          placeholder="Confirm"
+          textContentType={"password"}
+          onChangeText={(text) => setCheck(text)}
+          style={styles.input}
+          secureTextEntry={true}
+          maxLength={30}
+        ></TextInput>
+        <TouchableHighlight
+          style={styles.submitButton}
+          onPress={() => clickSubmit()}
         >
-          <Text
-            style={{
-              fontSize: 25,
-              color: "white",
-              padding: 16,
-              textAlign: "center",
-            }}
-          >
-            Login
-          </Text>
+          <Text style={styles.submitText}>Signup</Text>
         </TouchableHighlight>
       </View>
     </SafeAreaView>
@@ -70,14 +81,50 @@ export default function Signup() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    backgroundColor: "white",
+    height: "100%",
+    paddingTop: 25,
+  },
   input: {
-    height: 50,
+    height: 40,
     margin: 12,
     borderWidth: 1,
     borderColor: "#313359",
     padding: 10,
-    width: "90%",
+    width: "85%",
     borderRadius: 999,
     paddingLeft: 20,
+  },
+
+  form: {
+    width: "100%",
+    flex: 0,
+    alignContent: "center",
+    textAlign: "left",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: -50,
+  },
+
+  formTitle: {
+    fontSize: 35,
+    color: "#ED3B5B",
+    textAlign: "left",
+    marginBottom: 20,
+    width: "80%",
+  },
+  submitButton: {
+    width: "50%",
+    backgroundColor: "#ED3B5B",
+    borderRadius: 45,
+    marginTop: 20,
+  },
+  submitText: {
+    fontSize: 25,
+    color: "white",
+    padding: 16,
+    textAlign: "center",
   },
 });
