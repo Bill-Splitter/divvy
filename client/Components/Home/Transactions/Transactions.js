09fab889d3fc9ae900f836e2f098f92181edd2db
jthagerman <jthagerma@gmail.com>
2021-08-27 14:47:07 -0400

    trans

--- client/Components/Home/Transactions/Transactions.js
+++ client/Components/Home/Transactions/Transactions.js
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 import { AntDesign } from "@expo/vector-icons";
 import Banner2 from "../Banner2";
+import { fetchBillsThunk } from "../../../store/bill";
 
 export default Transactions = () => {
+  const dispatch = useDispatch()
+
+  const bills = useSelector((state) => state.bill.bills || [])
+  const user = useSelector((state) => state.user.id)
+  dispatch(fetchBillsThunk(user))
+
+
+  console.log("the bills", bills)
+
+
   return (
     <View style={{ flex: 0, backgroundColor: "white", height: "100%" }}>
       <Banner2 name={'Transactions'}/>
--- client/store/bill.js
+++ client/store/bill.js
@@ -3,14 +3,32 @@ import axios from "axios";
 // const ApiKey = "cca55ddcec88957";
 
 const SET_BILL = "SET_BILL";
+const SET_BILLS = "SET_BILLS";
 const instance = axios.create({ baseURL: "http://localhost:8080" });
 
 const initialState = {
   bill: {},
+  bills: [],
+};
+
+export const setBills = (bills) => {
+  return {
+    type: SET_BILLS,
+    bills,
+  };
+};
+
+export const fetchBillsThunk = (userId) => {
+  return async (dispatch) => {
+    try {
+      const billsArray = await instance.get(`api/bills/${userId}`);
+      billsArray = billsArray.data;
+      dispatch(setBills(billsArray));
+    } catch (error) {}
+  };
 };
 
 export const createBillThunk = (bill, userid, friends) => {
-  console.log(userid, "thunk");
   return async (dispatch) => {
     try {
       const newBill = await instance.post("api/bills/", {
@@ -27,6 +45,8 @@ export const createBillThunk = (bill, userid, friends) => {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case SET_BILLS:
+      return { ...state, bills: action.bills };
     default:
       return state;
   }
